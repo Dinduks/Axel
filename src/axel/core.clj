bb6176700439a84b13b73d154a3373d20e88bbad
Samy Dindane <samy@dindane.com>
2013-05-01 16:27:42 +0200

    Change funcs declaration order

--- src/axel/core.clj
+++ src/axel/core.clj
@@ -1,7 +1,28 @@
 (ns axel.core
   (:require [org.httpkit.client :as client]
             [axel.util :refer :all])
-  (:import (java.nio ByteBuffer)))
+  (:import (java.nio ByteBuffer))
+  (:gen-class))
+
+(declare create-promises)
+(declare download-file)
+(declare save-part-to-disk)
+
+(defn -main
+  [& args]
+  (let [dest "/tmp/samy"
+        url "http://samy.dindane.com/samy"
+        response @(client/head url {:headers {"Accept-Encoding" ""}})]
+    (cond
+      (= (:status response) 404) (println "Error: File not found (404)")
+      (= (:status response) 403) (println "Error: File forbidden (403)")
+      :else (let [headers (:headers response)
+                  cont-len (int (read-string (get headers :content-length)))
+                  parts-size (get-parts-size cont-len 4)
+                  promises (map-indexed (create-promises url cont-len dest) parts-size)
+                  file-channel-fut (get-allocated-file-channel dest cont-len)
+                  ƒ (download-file file-channel-fut)]
+              (pmap ƒ promises)))))
 
 (defn create-promises
   [url cont-len dest]
@@ -18,7 +39,18 @@
        :start-offset start-offset
        :index i})))
 
-(defn save-to-disk-ƒ
+(defn download-file
+  [file-channel-fut]
+  (fn
+    [promises-hash]
+    (let [content-in-bytes (:body @(:promise promises-hash))
+          ƒ (save-part-to-disk promises-hash content-in-bytes)
+          index (inc (:index promises-hash))]
+      (println "Finished downloading part" index)
+      (ƒ file-channel-fut)
+      ()))) ; TODO: Remove the last useless s-exp
+
+(defn save-part-to-disk
   [promises-hash content-in-bytes]
   (fn
     [fc-fut]
@@ -31,29 +63,3 @@
         (println "Finished writing part    " (inc (:index promises-hash)) "to disk.")
         ()))))
 
-(defn save-to-disk
-  [file-channel-fut]
-  (fn
-    [promises-hash]
-    (let [content-in-bytes (:body @(:promise promises-hash))
-          ƒ (save-to-disk-ƒ promises-hash content-in-bytes)
-          index (inc (:index promises-hash))]
-      (println "Finished downloading part" index)
-      (ƒ file-channel-fut)
-      ()))) ; TODO: Remove the last useless s-exp
-
-(defn -main
-  [& args]
-  (let [dest "/tmp/samy"
-        url "http://samy.dindane.com/samy"
-        response @(client/head url {:headers {"Accept-Encoding" ""}})]
-    (cond
-      (= (:status response) 404) (println "Error: File not found (404)")
-      (= (:status response) 403) (println "Error: File forbidden (403)")
-      :else (let [headers (:headers response)
-                  cont-len (int (read-string (get headers :content-length)))
-                  parts-size (get-parts-size cont-len 4)
-                  promises (map-indexed (create-promises url cont-len dest) parts-size)
-                  file-channel-fut (get-allocated-file-channel dest cont-len)
-                  ƒ (save-to-disk file-channel-fut)]
-              (pmap ƒ promises)))))
